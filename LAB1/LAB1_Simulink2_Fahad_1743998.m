7e11324ab5895bea0492764a35d594d39eb6596d
Fahad AlHarbi <101593347+jdknull@users.noreply.github.com>
2024-01-15 09:01:00 +0300

    Delete LAB1 directory

--- LAB1/LAB1_Simulink2_Fahad_1743998.m
+++
@@ -1,9 +0,0 @@
-t = 0:0.01:10; 
-frequency = 5; 
-amplitude = 1; 
-
-sine_wave = amplitude * sin(2 * pi * frequency * t);
-plot(t, sine_wave);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Sine Wave Plot');
